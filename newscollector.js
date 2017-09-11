0e4ed3f168b5a6e4513c17825436ed6a66b36f70
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-09-11 19:57:23 +0300

    Started home page

--- newscollector.js
+++ newscollector.js
@@ -1,13 +1,29 @@
-var express = require('express')
+let express = require('express')
+let handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-var app = express();
+let app = express();
+app.engine('handlebars',handlebars.engine);
 
+
+app.disable('x-powered-by');
+app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname +'/public'));
+
 app.get('/' , function(request,response){
-    response.send('Everything is ok');
+    response.render('home');
+});
+app.get('/redir' , function(request,response){
+    response.send('You should have been redirected to :'+ request.query['choice'])    
 });
 
+app.get('/about' , function(request,response){
+    response.render('about');
+});
+
+
+
 app.listen(app.get('port'),function(){
-    console.log("Project is running");
+    console.log("Project is running on http://localhost:"+ app.get('port'));
 });
