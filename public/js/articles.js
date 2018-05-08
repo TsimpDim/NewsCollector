3ed6b36d6c53235ef9dde6f5e3cc3f82ae8f6213
TsimpDim <dimitristsimpitas@yahoo.gr>
2018-05-08 12:31:09 +0300

    Made page more responsive

--- public/js/articles.js
+++ public/js/articles.js
@@ -4,7 +4,6 @@ $("document").ready(function(){
 
     $("#prim_head_link").on("click", function(){
         $('html, body').animate({scrollTop: 0}, 250);
-        $("#header").css("font-size","4em");
         $("#header").css("opacity","1");
         minifiedHeader = false;
     });
@@ -12,11 +11,9 @@ $("document").ready(function(){
     $(window).scroll(function() {
         if($(window).scrollTop() > 50) {
             minifiedHeader = true;
-            $("#header").css("font-size","2em");
             $("#header").css("opacity","0.7");
         }else{
             minifiedHeader = false;
-            $("#header").css("font-size","4em");
             $("#header").css("opacity","1");
         }
     });
